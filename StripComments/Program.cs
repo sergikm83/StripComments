a7de4daf196a765ca01324b5a805496ae012b677
Sergey Misko <sergik.m@outlook.com>
2022-05-17 22:46:55 +0500

    добавил ссылку на КАТА

--- StripComments/Program.cs
+++ StripComments/Program.cs
@@ -4,6 +4,7 @@ namespace StripComments
 {
     class Program
     {
+        // https://www.codewars.com/kata/51c8e37cee245da6b40000bd/train/csharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
