83932912b6acccdc490e676d865634cce628050f
sergik.m <sergik.m@outlook.com>
2022-05-18 14:43:28 +0400

    добавил ссылку на КАТА

--- StripComments/Program.cs
+++ StripComments/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace StripComments
 {
+    // https://www.codewars.com/kata/51c8e37cee245da6b40000bd/train/csharp
     class Program
     {
         static void Main(string[] args)
